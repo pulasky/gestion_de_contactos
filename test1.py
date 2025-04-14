4c9e47e5c765480530bfba320469a93ac6ab1049
Pulasky <pulasky01@outlook.es>
2025-04-14 20:39:17 +0200

    Eliminados archivos no deseados y configurado .gitignore

--- test1.py
+++
@@ -1,9 +0,0 @@
-def min(a,b):
-    if a <= b:
-        return a
-    else:
-        return b
-numero1= int(input("Ingrese un numero: "))
-numero2= int(input("Ingrese otro numero: "))
-print("El menor de los dos numeros es: ", min(numero1,numero2))
-    
